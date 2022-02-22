4ae24daf37a0ad115dcbb5de89f236e06a67fd61
Patrick Liang <pl204511194@yahoo.com>
2022-02-22 12:36:06 -0500

    commit tree 9bdfb56be15ed7202a5345db759ad52ab91acae4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array 
   # Your code here
+  array.map.with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet array
   # Your code here
+  array.map do |name|
+    "#{name[0].upcase}#{name[1..name.length-1]}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+short_words = ["puff", "go", "two"]
+
+def long_planeteer_calls array
   # Your code here
+  four_letters = array.find do |name|
+    name.length > 4
+  end
+  if four_letters
+    true
+  else false
+  end
 end
 
-def find_the_cheese# code an argument here
+snacks = ["crackers", "gouda", "thyme"]
+ingredients = ["garlic", "rosemary", "bread"]
+
+def find_the_cheese array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |name|
+    cheese_types.include?(name)
+  end
 end
+
+puts find_the_cheese(snacks)
